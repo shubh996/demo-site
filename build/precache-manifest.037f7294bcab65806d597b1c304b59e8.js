ee91f3003371aa55dd95e1e2a861c27c97b1633b
Shubham Jain <shubham.jain@nium.com>
2021-12-10 09:46:20 +0530

    second

---
+++ build/precache-manifest.037f7294bcab65806d597b1c304b59e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25da976afefc3983c21a50fd537a26fd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bed651e84cf92af0a101",
+    "url": "./static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e58a4a493c725a153387",
+    "url": "./static/css/main.265b21ca.chunk.css"
+  },
+  {
+    "revision": "bed651e84cf92af0a101",
+    "url": "./static/js/2.335924ca.chunk.js"
+  },
+  {
+    "revision": "850884bae7f992ffe8c679b29cfca96b",
+    "url": "./static/js/2.335924ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e58a4a493c725a153387",
+    "url": "./static/js/main.d3dcf6f6.chunk.js"
+  },
+  {
+    "revision": "d62cd825610c580401cb",
+    "url": "./static/js/runtime-main.6268b4f6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/index.js
@@ -0,0 +1,56 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import Display_Product from './Display_Product';
+import queryString from 'query-string'
+import firebase from "firebase"
+
+const gotoproduct =() =>{
+
+const query = queryString.parse(window.location.search)
+const uid = query.p_uid
+const p_uid = uid.split("/")
+const b_uid= uid.split("b_uid")
+
+
+
+return( <Display_Product p_uid={p_uid[0]} b_uid={b_uid[1]} />)
+
+}
+
+const gotohome =() =>{
+
+      const query = queryString.parse(window.location.search) 
+      var x = window.location.href.split(".")[0].split("//")[1] 
+
+      
+      
+     
+        return( <App    b_uid={"ujM5h3v39xDNLrAZRGg3"}  /> )
+      
+  
+  
+  }
+
+
+
+ReactDOM.render(
+
+  <React.StrictMode>
+    {
+      window.location.search.includes("p_uid") 
+      ?gotoproduct()
+      :gotohome()
+    }
+    
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
